02bab45eb4f0da740978b0eade9e72227cc27cd4
nqhieu2005 <hieunguyen2005q@gmail.com>
2025-02-06 18:22:52 +0700

    lab1, ss1

--- Class1/server.js
+++ Class1/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Welcome!</h1><p>You are using the browser: ${req.headers[user-agent]}</p>');
+    res.end(`<h1>Welcome!</h1><p>You are using the browser: ${req.headers[user-agent]}</p>`);
 });
 
 server.listen(3000, () => {
