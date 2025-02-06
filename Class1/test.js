99e5a0c1a2ffa4689610e47cd7a40de23ba1de95
nqhieu2005 <hieunguyen2005q@gmail.com>
2025-02-06 19:09:40 +0700

    add index html

--- Class1/test.js
+++ Class1/test.js
@@ -1,7 +1,21 @@
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello world");
-}).listen(3000);
-console.log('Server is running at http://localhost:3000/');
+// http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end("Hello world");
+// }).listen(3000);
+// console.log('Server is running at http://localhost:3000/');
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+});
+
+server.listen(3000, ()=>{
+    console.log("Server is running at http://localhost:3000");
+});
